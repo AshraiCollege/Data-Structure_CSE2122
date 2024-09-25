a1b1448febf9e2df959761d399557ab98f5b253f
Devit Chandra Roy <ashraicselab@gmail.com>
2024-09-25 10:36:09 +0600

    Update Lab_13_shortestPath.c

--- Lab_13_shortestPath.c
+++ Lab_13_shortestPath.c
@@ -1,96 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
-#define MAX_CITIES 100
-#define INF INT_MAX
-
-void initializeGraph(int graph[MAX_CITIES][MAX_CITIES], int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                graph[i][j] = 0; // Distance to itself is zero
-            } else {
-                graph[i][j] = INF; // Initialize other distances to infinity
-            }
-        }
-    }
-}
-
-void addEdge(int graph[MAX_CITIES][MAX_CITIES], int u, int v, int weight) {
-    graph[u][v] = weight; // For directed graph, set distance
-    graph[v][u] = weight; // Uncomment for undirected graph
-}
-
-void printGraph(int graph[MAX_CITIES][MAX_CITIES], int n) {
-    printf("Adjacency Matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (graph[i][j] == INF) {
-                printf("INF\t");
-            } else {
-                printf("%d\t", graph[i][j]);
-            }
-        }
-        printf("\n");
-    }
-}
-
-void floydWarshall(int graph[MAX_CITIES][MAX_CITIES], int n) {
-    int dist[MAX_CITIES][MAX_CITIES];
-
-    // Initialize distance array
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            dist[i][j] = graph[i][j];
-        }
-    }
-
-    // Warshall's algorithm
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-
-    // Print shortest paths
-    printf("Shortest distances between every pair of cities:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF) {
-                printf("INF\t");
-            } else {
-                printf("%d\t", dist[i][j]);
-            }
-        }
-        printf("\n");
-    }
-}
-
-int main() {
-    int n, u, v, weight;
-    int graph[MAX_CITIES][MAX_CITIES];
-
-    printf("Enter the number of cities: ");
-    scanf("%d", &n);
-
-    initializeGraph(graph, n);
-
-    // Add edges
-    while (1) {
-        printf("Enter edge (u v weight) or -1 to stop: ");
-        scanf("%d", &u);
-        if (u == -1) break;
-        scanf("%d %d", &v, &weight);
-        addEdge(graph, u, v, weight);
-    }
-
-    printGraph(graph, n);
-    floydWarshall(graph, n);
-
-    return 0;
-}
+#include <stdio.h>  
+#define INF 2147483647  
+   
+int n = 5; // size of the adjaceny matrix  
+   
+// O(n^2)  
+void fillDistanceMatrix(int A[n][n], int D[n][n]) {  
+   for (int i = 0; i < n; i++) {  
+       for (int j = 0; j < n; j++) {  
+           if (i == j)  
+               D[i][j] = 0;  
+           else if (A[i][j] == 0)  
+               D[i][j] = INF;  
+           else  
+               D[i][j] = A[i][j];  
+       }  
+   }  
+}  
+   
+// O(n^3)  
+void floydWarshall(int A[n][n], int D[n][n]) {  
+   fillDistanceMatrix(A, D);  
+   
+   for (int k = 0; k < n; k++)
+   {  
+       for (int i = 0; i < n; i++) 
+       {  
+           for (int j = 0; j < n; j++)
+           {  
+               //if (D[i][k] < INF && D[k][j] < INF) 
+               if(D[i][j] > (D[i][k] + D[k][j]) && (D[k][j] != INF
+                        && D[i][k] != INF))
+               {
+                   if (D[i][k] + D[k][j] < D[i][j]) 
+                   {
+                       D[i][j] = D[i][k] + D[k][j];
+                    }
+               }  
+           }  
+       }  
+   }  
+}  
+int main() {  
+    int A[5][5] = {{0, 5, 0,6,0},  
+                   {0, 0, 1,0,7},  
+                   {3, 0, 0,4, 0},  
+                   {0, 0, 2, 0,3},
+                   {2, 0, 0,5, 0}};  
+    int D[5][5];  
+   
+    floydWarshall(A, D);  
+   
+    printf("Shortest distances between all pairs of vertices:\n");  
+    for (int i = 0; i < 5; i++) {  
+        for (int j = 0; j < 5; j++) {  
+            if (D[i][j] == INF)  
+                printf("%7s", "INF");  
+            else  
+                printf("%7d", D[i][j]);  
+        }  
+        printf("\n");  
+    }  
+   
+    return 0;  
+}  
